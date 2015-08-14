de7cbc221605d5b5bef8e921ca79156eea4b45e7
CoyoteBringsFire <aumkara@gmail.com>
2015-08-14 15:37:52 -0700

    mocha tests

---
+++ test/index.js
@@ -0,0 +1,17 @@
+var should 	= require('should'),
+	util 	= require('util');
+
+var requiredProperties = [ "os_type", "os_platform", "os_arch", "os_release", "os_uptime", "os_loadavg", "os_freemem", "os_totalmem", "os_cpus", "process_env", "process_version", "process_versions", "process_config",  "process_memoryUsage", "process_umask" ]
+
+describe("env-snapshot", function() {
+	var envSnapshot 	= require('../').EnvSnapshot();
+
+	for( property in requiredProperties ) {
+		(function() {
+			var prop =	requiredProperties[property];
+			it( util.format("should have %s in the response", prop), function doIt() {
+				should(envSnapshot[prop]).be.ok;
+			});
+		})();
+	}
+})
